c7187af76a140f0dadfca2abe26a816b4c086e9d
horange89 <sbo78@naver.com>
2020-02-15 14:13:02 +0900

    Commit Test

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/EatgoApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.co.fastcampus.eatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class EatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
