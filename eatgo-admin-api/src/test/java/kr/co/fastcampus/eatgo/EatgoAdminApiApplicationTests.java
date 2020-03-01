5b43cf9a2626f9b333ec8a36cf9536601a390924
horange89 <sbo78@naver.com>
2020-03-01 23:51:17 +0900

    프로젝트분할 : 기존에 사용하던 domain을 Common 공통 모듈로 따로 분리하였고 고객용과 관리자용 api를 나누기 위해서 작업

--- eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgo/EatgoAdminApiApplicationTests.java
+++ eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgo/EatgoAdminApiApplicationTests.java
@@ -2,10 +2,9 @@ package kr.co.fastcampus.eatgo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class EatgoApplicationTests {
+class EatgoAdminApiApplicationTests {
 
 	@Test
 	void contextLoads() {
