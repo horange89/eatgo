c0bfef97e492db36b3c56db378c418a70a222cd8
horange89 <sbo78@naver.com>
2020-03-09 23:10:42 +0900

    제목 : Category 추가 내용 : Category 관련 기능 추가

---
+++ eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/CategoryTest.java
@@ -0,0 +1,16 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+class CategoryTest {
+
+    @Test
+    public void creation() {
+        Category category = Category.builder().name("Korean Food").build();
+
+        assertThat(category.getName(), is("Korean Food"));
+    }
+}
