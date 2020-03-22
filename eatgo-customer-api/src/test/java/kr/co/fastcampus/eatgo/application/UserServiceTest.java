ec19477c22f4504141f9d87188e209ca3c58d1e0
horange89 <sbo78@naver.com>
2020-03-22 23:59:04 +0900

    제목 : 회원관리 비밀번호 기능 추가 내용 : 회원 추가에 필요한 컨트롤러 및 서비스 구현

---
+++ eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/UserServiceTest.java
@@ -0,0 +1,37 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+class UserServiceTest {
+
+
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    public void registerUser() {
+        String email = "tester@example.com";
+        String name = "Tester";
+        String password = "test";
+
+        userService.registerUser(email, name, password);
+
+        verify(userRepository).save(any());
+    }
+}
